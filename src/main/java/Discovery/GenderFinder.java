431865f2874651f4fdc4a636afe38ab14caa3653
xuexiaoying <camilla.xue@mail.utoronto.ca>
2022-11-06 21:28:04 -0500

    Some documentations written on UserPreference class that Camilla thinks would be helpful for PreferenceFinder. Before implementing actual body for methods, hopefully group members can help discuss the possibility of the method.

---
+++ src/main/java/Discovery/GenderFinder.java
@@ -0,0 +1,41 @@
+package Discovery;
+
+import java.util.ArrayList;
+
+/**
+ * GenderFinder takes user's sexual orientation and filter other users based
+ * Documentary of possible sexual orientation:
+ *(possible sexual orientation): (return from GenderFinder)
+ * male: return only users identified themselves as male
+ * female: return only users identified themselves as female
+ * bisexual: return only users identified themselves as both male and female
+ * transgender: return only users identified themselves as transgender
+ * open: return all the users
+ */
+
+public class GenderFinder {
+    String userChoice = User.getSexualOrientation();
+    public ArrayList<User> getList(ArrayList<User> otherUser){
+        ArrayList<User> res = new ArrayList<User>();
+        //male,female, transgender cases
+        if (userChoice.equals("male")||
+                userChoice.equals("female")||
+                userChoice.equals("transgender")){
+            for (User otherUser:otherUser){
+                //user.sexualOrientation is variable recording user's sexualOrientation
+                if (otherUser.getSexualOrientation().equals(userChoice)){res.add(otherUser);}
+            }
+            return res;
+        }
+        //bisexual cases
+        if (userChoice.equals("bisexual")){
+            for (User otherUser:otherUser){
+                if (otherUser.getSexualOrientation().equals("male")||
+                        otherUser.getSexualOrientation().equals("female")){res.add(otherUser);}
+            }
+            return res;
+        }
+        //open case
+        return otherUser;
+    }
+}
