6128a1db619adcd699fb3a01c6e633bf15d6d6b9
xuexiaoying <camilla.xue@mail.utoronto.ca>
2022-12-06 20:15:37 -0500

    Updates on database, added users with different interestRank.

--- src/main/java/use_case/discovery/DiscoveryResponseModel.java
+++ src/main/java/use_case/discovery/DiscoveryResponseModel.java
@@ -14,19 +14,19 @@ public class DiscoveryResponseModel {
     }
 
     public String getUserName1(){return this.userNames.get(0);}
-//    public String getUserName2(){return this.userNames.get(1);}
-//    public String getUserName3(){return this.userNames.get(2);}
-//    public String getUserName4(){return this.userNames.get(3);}
-//    public String getUserName5(){return this.userNames.get(4);}
-//    public String getUserName6(){return this.userNames.get(5);}
-//    public String getUserName7(){return this.userNames.get(6);}
-//    public String getUserName8(){return this.userNames.get(7);}
-//    public String getUserName9(){return this.userNames.get(8);}
-//    public String getUserName10(){return this.userNames.get(9);}
-//    public String getUserName11(){return this.userNames.get(10);}
-//    public String getUserName12(){return this.userNames.get(11);}
-//    public String getUserName13(){return this.userNames.get(12);}
-//    public String getUserName14(){return this.userNames.get(13);}
-//    public String getUserName15(){return this.userNames.get(14);}
+    public String getUserName2(){return this.userNames.get(1);}
+    public String getUserName3(){return this.userNames.get(2);}
+    public String getUserName4(){return this.userNames.get(3);}
+    public String getUserName5(){return this.userNames.get(4);}
+    public String getUserName6(){return this.userNames.get(5);}
+    public String getUserName7(){return this.userNames.get(6);}
+    public String getUserName8(){return this.userNames.get(7);}
+    public String getUserName9(){return this.userNames.get(8);}
+    public String getUserName10(){return this.userNames.get(9);}
+    public String getUserName11(){return this.userNames.get(10);}
+    public String getUserName12(){return this.userNames.get(11);}
+    public String getUserName13(){return this.userNames.get(12);}
+    public String getUserName14(){return this.userNames.get(13);}
+    public String getUserName15(){return this.userNames.get(14);}
 }
 
