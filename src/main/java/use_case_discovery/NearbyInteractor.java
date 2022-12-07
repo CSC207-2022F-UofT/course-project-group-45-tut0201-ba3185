609d9ed0445cf06189b1f9920616a739379985c5
zzy620 <zyuan.zhang@mail.utoronto.ca>
2022-12-07 16:38:48 -0500

    update OptionPanel

--- src/main/java/use_case_discovery/NearbyInteractor.java
+++ src/main/java/use_case_discovery/NearbyInteractor.java
@@ -9,7 +9,7 @@ public class NearbyInteractor implements DiscoveryInputBoundary {
      */
     public NearbyInteractor(DiscoveryListInterface pPanel){
         this.presenter = pPanel;
-        this.dResponseModel = new DiscoveryResponseModel(new PreferenceListHelper().getList());
+        this.dResponseModel = new DiscoveryResponseModel(new GetNearbyUserHelper().getList());
     }
     @Override
     public void create() {
