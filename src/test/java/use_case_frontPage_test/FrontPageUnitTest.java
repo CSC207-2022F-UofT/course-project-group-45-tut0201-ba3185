eb72e9460663f3839008e7566fe2ddc2f02cc3eb
alyssali <alyssamengyuan.li@mail.utoronto.ca>
2022-12-08 10:13:17 -0500

    delete unnecessary docstring

--- src/test/java/use_case_frontPage_test/FrontPageUnitTest.java
+++ src/test/java/use_case_frontPage_test/FrontPageUnitTest.java
@@ -15,7 +15,6 @@ import use_case_message.MessageManagers;
  * Unit tests for the FrontPageInteractor class, which tests the case when creating a front page with username button on it.
  * In this case, the current user doesn't chat with the targetuser, thus there shouldn't be any chatted user button of it,
  * by examining the output data we obtained in responseModel passed in presenter.
- * Since this test doesn't involve in modifying database, there is no use to load and reload before and after test execution.
  * The ideal test design should also include the case that targetuser and current user have already chatted, an issue will be addressed
  * in repo and summary of testing method will be included.
  * The connection of each component of clean architecture is well tested, however, presenter, controller and view is not included.
