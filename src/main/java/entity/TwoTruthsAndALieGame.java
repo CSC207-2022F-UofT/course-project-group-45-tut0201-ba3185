6fcb6ea6357c5a57a79ce17466564b766991214b
liuyaqi <imamber.liu@mail.utoronto.ca>
2022-12-08 11:34:28 -0500

    modify TwoTruthsAndALieGame

--- src/main/java/entity/TwoTruthsAndALieGame.java
+++ src/main/java/entity/TwoTruthsAndALieGame.java
@@ -4,7 +4,7 @@ import java.sql.Statement;
 
 /**
  *  Two Truths And A Lie Game Entity
- *  Keeps track of the details of Two Truths And A Lie game, such as players. （Further detail is tracked within each player)
+ *  Keeps track of the details of Two Truths And A Lie game, such as players. Further detail is tracked within each player
  *  @author  Eric Xue
  */
 public class TwoTruthsAndALieGame {
