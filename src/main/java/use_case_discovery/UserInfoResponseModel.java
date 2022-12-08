2cfecafe90436c310373acdc978bf1c893fc28aa
zzy620 <zyuan.zhang@mail.utoronto.ca>
2022-12-08 08:14:23 -0500

    update

---
+++ src/main/java/use_case_discovery/UserInfoResponseModel.java
@@ -0,0 +1,40 @@
+package use_case_discovery;
+
+public class UserInfoResponseModel {
+    private int age;
+    private String name;
+    private String hobby;
+    private String relationshipType;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setHobby(String hobby){
+        this.hobby = hobby;
+    }
+    public String getHobby(){
+        return hobby;
+    }
+    public void setRelationshipType(String relationshipType){
+        this.relationshipType = relationshipType;
+    }
+
+    public String getRelationshipType(){
+        return relationshipType;
+    }
+
+
+}
