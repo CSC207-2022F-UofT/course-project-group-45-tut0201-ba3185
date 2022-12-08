1c5f2d1021ac2d584f929d8ab27772922b137861
alyssali <alyssamengyuan.li@mail.utoronto.ca>
2022-12-08 01:16:52 -0500

    modified unit test

--- src/test/java/use_case_frontPage_test/FrontPageUnitTest.java
+++ src/test/java/use_case_frontPage_test/FrontPageUnitTest.java
@@ -9,7 +9,12 @@ import use_case_frontpage.FrontPageResponseModel;
 
 /**
  * Unit tests for the FrontPageInteractor class, which tests the case when creating a front page with username button on it.
- * The connection of each component of clean architecture is well tested, however, view is not included.
+ * In this case, the current user doesn't chat with the targetuser, thus there shouldn't be any chatted user button of it,
+ * by examining the output data we obtained in responseModel passed in presenter.
+ * Since this test doesn't involve in modifying database, there is no use to load and reload before and after test execution.
+ * The ideal test design should also include the case that targetuser and current user have already chatted, an issue will be addressed
+ * in repo and summary of testing method will be included.
+ * The connection of each component of clean architecture is well tested, however, presenter, controller and view is not included.
  *
  */
 public class FrontPageUnitTest {
@@ -18,7 +23,7 @@ public class FrontPageUnitTest {
         FrontPageOutputBoundary frontPageOutputBoundary = new FrontPageOutputBoundary() {
             @Override
             public void create(FrontPageResponseModel responseModel) {
-                Assertions.assertTrue(responseModel.getTargetUsers().contains("Jenny"));
+                Assertions.assertFalse(responseModel.getTargetUsers().contains("Jenny"));
             }
         };
         FrontPageInteractor interactor = new FrontPageInteractor(frontPageOutputBoundary);
